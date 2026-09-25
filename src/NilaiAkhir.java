ba6a62a9a3a0952117d4a5b46bbaea59ad091f09
Kesia Kristina Nababan <2581031@unai.edu>
2026-09-25 09:49:27 +0700

    Menambahkan setup project dan 4 bobot konstanta

---
+++ src/NilaiAkhir.java
@@ -0,0 +1,13 @@
+ import java.util.Scanner;
+ public class NilaiAkhir {
+
+     // Empat bobot disimpan sebagai konstanta, bukan diketik langsung didalam rumusnya
+     static final double BOBOT_PRAKTIKUM = 0.30;
+     static final double BOBOT_TUGAS = 0.20;
+     static final double BOBOT_MID = 0.20;
+     static final double BOBOT_FINAL = 0.30;
+
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+     }
